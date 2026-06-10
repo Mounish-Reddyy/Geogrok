606949a8b7ed0edc0314d939cc4868ab8eee1820
Mounish-Reddyy <mounishgangula194@gmail.com>
2026-06-11 00:18:11 +0530

    Add files via upload

---
+++ rps3.js
@@ -0,0 +1,140 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the Capital of Armenia?",
+    answer: "Yerevan",
+    options: [
+      "Baku",
+      "Yerevan",
+      "Tbilisi",
+      "Bishkek"
+    ]
+  },
+     
+  
+    {
+    numb: 2,
+    question: "Which is Largest state in USA?",
+    answer: "Alaska",
+    options: [
+      "Texas",
+      "Alaska",
+      "California",
+      "Florida"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Chad in which Continent?",
+    answer: "Africa",
+    options: [
+      "Asia",
+      "Europe",
+      "Africa",
+      "North America"
+    ]
+  },
+      
+  
+    {
+    numb: 4,
+    question: "What is full form of HDI?",
+    answer: "Human Development Index",
+    options: [
+      "Human Developing Index",
+      "Human Developing Index",
+      "Horse Development Index",
+      "Human Development Index"
+    ]
+  },
+   
+     {
+    numb: 5,
+    question: "Largest City in Ukraine?",
+    answer: "Kyiv",
+    options: [
+      "Kyiv",
+      "Kharkiv",
+      "Dnipro",
+      "Odesa"
+    ]
+  },
+
+     {
+    numb: 6,
+    question: "Largest City in Venezuela?",
+    answer: "Caracas",
+    options: [
+      "Valencia",
+      "Maracaibo",
+      "Caracas",
+      "Barquisimeto"
+    ]
+  },
+
+     {
+    numb: 7 ,
+    question: "Largest Lake in the World?",
+    answer: "Caspian Sea",
+    options: [
+      "Dead Sea",
+      "Lake Superior",
+      "Lake Victoria",
+      "Caspian Sea"
+    ]
+  },
+
+     {
+    numb: 8,
+    question: "Which one of following country doesn't exist?",
+    answer: "Scotland",
+    options: [
+      "Scotland",
+      "Iceland",
+      "Finland",
+      "Norway"
+    ]
+  },
+
+    {
+    numb: 9,
+    question: "Paris of Middle East?",
+    answer: "Beirut",
+    options: [
+      "Damascus",
+      "Jeddah",
+      "Beirut",
+      "Jerusalem"
+    ]
+  },
+
+     {
+    numb: 10,
+    question: "Most Dangerous Country in Asia?",
+    answer: "Afghanistan",
+    options: [
+      "India",
+      "Iraq",
+      "Afghanistan",
+      "Yemen"
+    ]
+  },
+
+    
+      
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
